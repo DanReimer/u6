1f3cdb3a5a389cc447e6f730fd52608bc29d645b
DanReimer <dajudoguy@gmail.com>
2018-03-14 17:01:03 +0000

    Finished part 3. Added province model. Added province to customer model as reference (1 to many).

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312180119) do
+ActiveRecord::Schema.define(version: 20180314163558) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -51,6 +51,14 @@ ActiveRecord::Schema.define(version: 20180312180119) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "notes"
+    t.integer "province_id"
+    t.index ["province_id"], name: "index_customers_on_province_id"
+  end
+
+  create_table "provinces", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
