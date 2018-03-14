a857f598aad97d5cfea6f3869e04321fa8eadbb9
DanReimer <dajudoguy@gmail.com>
2018-03-14 15:39:38 +0000

    Added customers controller, views, and routes. Views not rendering properly.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'customers#index'
+  get '/customers/missing_email', to: 'customers#missing_email'
+  get '/customers/alphabetized', to: 'customers#alphabetized'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
